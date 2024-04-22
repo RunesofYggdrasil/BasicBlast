10d40247571d292be0b9b58fe8dddf7216a5837f
RunesofYggdrasil <rookmarcus79@gmail.com>
2024-04-22 11:53:33 -0400

    Mobile Compatible Nav Bar

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import React from "react";
 import styles from "./Navigation.module.css";
 import Link from "next/link";
 
 const Navigation = () => {
   // Replace Logo Image
-  //   Could make a pun about how ROSEARCH could be Ro Search or Rose Arch
+  // Could make a pun about how ROSEARCH could be Ro Search or Rose Arch
   return (
     <nav className={styles.navigationBar}>
       <div className={styles.navigationLeft}>
--- app/components/SequenceCard.tsx
+++ app/components/SequenceCard.tsx
@@ -7,32 +7,15 @@ interface SequenceProps {
 }
 
 const SequenceCard = ({ sequence, sequenceID }: SequenceProps) => {
-  const sequenceArray: string[] = [];
-  sequenceArray.length = 0;
-  let arrayIndex = 0;
-  for (
-    let sequenceIndex = 0;
-    sequenceIndex < sequence.length;
-    sequenceIndex += 100
-  ) {
-    let sequenceSubset = sequence.substring(sequenceIndex, sequenceIndex + 100);
-    sequenceArray[arrayIndex] = sequenceSubset;
-    arrayIndex++;
-  }
-
   return (
     <>
       <div className={styles.sequenceData}>
         <p>{sequence.length}</p>
       </div>
       <div className={styles.sequenceAlignment}>
-        {sequenceArray.map((subset) => {
-          return (
-            <div className={styles.rowAlignment}>
-              <p>{subset}</p>
-            </div>
-          );
-        })}
+        <div className={styles.rowAlignment}>
+          <p>{sequence}</p>
+        </div>
       </div>
     </>
   );
