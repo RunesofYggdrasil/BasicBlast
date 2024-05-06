031188d435107c2e1f75d4c17be3618cb0b5a123
RunesofYggdrasil <rookmarcus79@gmail.com>
2024-05-06 14:51:02 -0400

    Router Kind Of

---
+++ app/api/users/route.ts
@@ -0,0 +1,11 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const GET = async (req: NextRequest) => {
+  const users = await prisma.user.findMany();
+  return NextResponse.json({
+    users,
+  });
+};
+
+export { GET };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useRouter } from "next/navigation";
+
+const getUsers = async () => {
+  const router = useRouter();
+  const users = await fetch("/api/users", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: "nothin" }),
+  });
+  router.push("/");
+  return users;
+};
+
+export default async function Home() {
+  const users = await getUsers();
+  console.log(users);
   return (
     <main>
       <h1>hi</h1>
